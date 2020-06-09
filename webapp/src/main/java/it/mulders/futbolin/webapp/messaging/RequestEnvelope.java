162a3aa1f1b106adc5653a86a833a93cca54bfa1
Maarten Mulders <m.th.mulders@xs4all.nl>
2020-06-09 20:45:09 +0200

    Make envelope fields visible outside own package

--- webapp/src/main/java/it/mulders/futbolin/webapp/messaging/RequestEnvelope.java
+++ webapp/src/main/java/it/mulders/futbolin/webapp/messaging/RequestEnvelope.java
@@ -8,11 +8,11 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 public class RequestEnvelope {
     /** An unique identifier for the message. The response message will have the same correlationId, so they can be matched. */
-    final String correlationId;
+    public final String correlationId;
     /** Name of the queue where the consumer expects the response message to be published. */
-    final String responseQueueName;
+    public final String responseQueueName;
     /** Raw content of the message. */
-    final byte[] message;
+    public final byte[] message;
 
     /**
      * Convenience constructor for "fire-and-forget" message style, where the consumer does not expect a response
